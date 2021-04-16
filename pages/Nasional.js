c2b0487456c4f7031c70a190b50b1db857a4836c
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-04-17 00:16:18 +0700

    update

--- pages/Nasional.js
+++ pages/Nasional.js
@@ -2,63 +2,8 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const scrape = require('cheerio');
-const jwt = require('jsonwebtoken');
 
 const url = 'https://nasional.sindonews.com/';
-
-
-function aktifasi(req, res, next)
-{
-    const headers = req.headers['authorization'];
-
-    if(typeof headers !== 'undefined')
-    {
-        const blast = headers.split(" ");
-        const result = blast[1];
-        req.token = result;
-        next();
-    }else{
-        res.status(403);
-        res.json({
-            status: 403,
-            pesan: 'Need An Authorization Param',
-        })
-    }
-}
-
-router.get('/rahasia', aktifasi, (req, res, next) => {
-    jwt.verify(req.token, 'secret_data', (err, data) => {
-        if( err )
-        {
-            res.status(403);
-            res.json({
-                status: 403,
-                pesan: err,
-            })
-        }else{
-
-            res.status(200).json({
-                pesan: 'Jangan berisik ini halaman rahasia...!!',
-                result: data,
-            })
-        }
-    })
-});
-
-router.post('/login', (req, res) => {
-    const data = {
-        nama: 'Rizki Maulana',
-        images: 'thisImg.jpg',
-        url: 'https://www.lagukita.net',
-    }
-
-    const token = jwt.sign(data, 'secret_data');
-    res.json({
-        status: 200,
-        pesan: 'Already to go',
-        token: token,
-    })
-})
 
 router.get('/', (req, res) => {
     res.status(200);
