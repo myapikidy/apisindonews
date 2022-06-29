663cb1ba180be07424a3faa3831c7a27118fc1c2
myapikidy <108398135+myapikidy@users.noreply.github.com>
2022-06-29 16:19:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 
 server.listen(port, (err) => {
     if(err) throw err;
